dcd2d06b8bdc283b10812a265d31835691b5f316
Jigar Panchal <panchal.jigar90@gmail.com>
2019-04-06 11:46:27 +0530

    Update Row_vise_filter.R

--- Row_vise_filter.R
+++ Row_vise_filter.R
@@ -15,3 +15,20 @@ vvv <- rowSums(is.na(aaaaa))
 index_no <- names(vvv[vvv <= (ncol(temp)-1)-min_select])
 aaaaa[index_no,1]
 }
+
+
+
+
+find_top <- function(value_above=5, min_select = 4){
+  temp <- read.csv("Phenotype.csv",header = T)
+  use_df <- sapply(temp[,2:ncol(temp)], function(x){ y <- value_above:10;match(x, y)})
+  tidy_df <- cbind(temp$SampleID, use_df)
+  NA_df_filter <- rowSums(is.na(tidy_df))
+  names(NA_df_filter) <- 1:nrow(temp)
+  index_no <- names(NA_df_filter[NA_df_filter <= (ncol(temp)-1)-min_select])
+  tidy_df[index_no,1]
+}
+
+
+
+names(rowSums(is.na(cbind(temp$SampleID,(sapply(temp[,2:ncol(temp)], function(x){ y <- 4:10;match(x, y)}))))))
