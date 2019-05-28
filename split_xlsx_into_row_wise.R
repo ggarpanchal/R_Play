d5ebaab6f78676f1f0f24deb4f4b06ef0cce48e1
Jigar Panchal <panchal.jigar90@gmail.com>
2019-05-28 16:36:29 +0530

    Create split_xlsx_into_row_wise.R

---
+++ split_xlsx_into_row_wise.R
@@ -0,0 +1,18 @@
+library(xlsx)
+library(dplyr)
+main <- read.xlsx("Sticker_Sheet.xlsx", header = T, sheetIndex = 1)
+
+
+for ( i in unique(main$Standard)){
+  print(i)
+  filter_DF <- main %>%
+                filter(Standard == i)
+  for( j in unique(filter_DF$Section)) {
+    print(j)
+    final_DF <<- filter_DF %>%
+        filter(Section == j) %>% 
+        select(3:4)
+    filename_xlsx <- paste(i, "_",j,".csv",sep = "")
+    write.csv2(file = filename_xlsx,x = final_DF,row.names = T, col.names = T)
+  }
+}
