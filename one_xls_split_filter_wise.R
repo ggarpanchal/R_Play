cb2ac9d2c7b442ce6880cd2dfde9f20cc196ac58
Jigar Panchal <panchal.jigar90@gmail.com>
2019-06-14 18:23:36 +0530

    Update one_xls_split_filter_wise.R

--- one_xls_split_filter_wise.R
+++ one_xls_split_filter_wise.R
@@ -1,18 +1,28 @@
-library(xlsx)
-library(dplyr)
+# Author :- Jigar Panchal
+# Github :- ggarpanchal
+# Require Packages :- dplyr ( Tidyverse ), xlsx
+
+if (!require('xlsx')) install.packages('xlsx')
+if (!require('dplyr')) install.packages('dplyr')
+
+library('xlsx')
+library('dplyr')
+
 main <- read.xlsx("Sticker_Sheet.xlsx", header = T, sheetIndex = 1)
 
-
 for ( i in unique(main$Standard)){
-  print(i)
+  
   filter_DF <- main %>%
-                filter(Standard == i)
+    filter(Standard == i)
+  
   for( j in unique(filter_DF$Section)) {
-    print(j)
-    final_DF <<- filter_DF %>%
-        filter(Section == j) %>% 
-        select(3:4)
+    
+    final_DF <- filter_DF %>%
+      filter(Section == j) %>% 
+      select(3:4)
+    
     filename_xlsx <- paste(i, "_",j,".csv",sep = "")
-    write.csv(file = filename_xlsx,x = final_DF,row.names = F, col.names = T,sep = ",",quote = F)
+    
+    write.csv(file = filename_xlsx, x = final_DF, row.names = F, col.names = T, sep = ",", quote = F )
   }
 }
